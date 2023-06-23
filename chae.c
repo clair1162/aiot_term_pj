e0970ce63a21c839ec88e9f54ca8edf2590c572e
clair1162 <clair1162@khu.ac.kr>
2023-06-23 14:19:53 +0000

    all complete

--- chae.c
+++ chae.c
@@ -162,6 +162,13 @@ int main() {
     printf("Content-type:text/html\n\n");
     printf("<html>\n<head>\n<title>STOP WATCH PROGRAM</title>\n</head>\n");
     printf("<body>\n<p>Stop Watch</p>\n</br>\n");
+    printf("<form method=get action='chae.cgi'>\n");
+   printf("<input type='submit' name='button' value='Start'>\n</form>\n");
+   printf("<form method=get action='num_st.cgi'>\n");
+   printf("<input type='submit' name='button' value='Stop'>\n</form>\n");
+   printf("<form method=get action='num_cl.cgi'>\n");
+   printf("<input type='submit' name='button' value='Clear'>\n</form>\n");
+   printf("</body>\n</html>");
 
     pthread_t fndThread;
     int result;
