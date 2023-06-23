50adb1f8127a7b398f71668edf13600a0362ed2c
clair1162 <clair1162@khu.ac.kr>
2023-06-23 13:24:55 +0000

    start, stop, clear all complete

--- num2.c
+++ num2.c
@@ -4,13 +4,11 @@
 #include <string.h>
 #include <math.h>
 #include <fcntl.h>
-//#include <pthread.h>
+#include <pthread.h>
 #include <wiringPi.h>
-
 
 #define FIFO_FILE   "/tmp/fifo"
 #define BUFF_SIZE   1024
-
 
 const int FndSelectPin[6] = {4, 17, 18, 27, 22, 23};
 const int FndPin[8] = {6, 12, 13, 16, 19, 20, 26, 21};
@@ -18,231 +16,161 @@ const int FndFont[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0
 int pos[6] = {0, 0, 0, 0, 0, 0};
 int timer = 0;
 bool stop = false;
-
+pthread_mutex_t mutex;
 
 void Setup() {
-  int i;
-  //int fd;
+    int i;
 
-  if (wiringPiSetupGpio() == -1) {
-    printf("wiringPiSetupGpio() error\n");
-    exit(-1);
-  }
+    if (wiringPiSetupGpio() == -1) {
+        printf("wiringPiSetupGpio() error\n");
+        exit(-1);
+    }
 
-  for (i = 0; i < 6; i++) {
-    pinMode(FndSelectPin[i], OUTPUT);
-    digitalWrite(FndSelectPin[i], HIGH);
-  }
+    for (i = 0; i < 6; i++) {
+        pinMode(FndSelectPin[i], OUTPUT);
+        digitalWrite(FndSelectPin[i], HIGH);
+    }
 
-  for (i = 0; i < 8; i++) {
-    pinMode(FndPin[i], OUTPUT);
-    digitalWrite(FndPin[i], LOW);
-  }
+    for (i = 0; i < 8; i++) {
+        pinMode(FndPin[i], OUTPUT);
+        digitalWrite(FndPin[i], LOW);
+    }
 
-  if (access(FIFO_FILE, F_OK) == -1) {
-      mkfifo(FIFO_FILE, 0666);
-  }
+    if (access(FIFO_FILE, F_OK) == -1) {
+        mkfifo(FIFO_FILE, 0666);
+    }
 
-   stop = false;
+    stop = false;
 }
-
 
 void FndSelect(int position) {
-  int i;
+    int i;
 
-  for (i = 0; i < 6; i++) {
-    if (i == position) {
-      digitalWrite(FndSelectPin[i], LOW);
+    for (i = 0; i < 6; i++) {
+        if (i == position) {
+            digitalWrite(FndSelectPin[i], LOW);
+        }
+        else {
+            digitalWrite(FndSelectPin[i], HIGH);
+        }
     }
-    else {
-      digitalWrite(FndSelectPin[i], HIGH);
-    }
-  }
 }
-
 
 void FndDisplay(int position, int num) {
-  int i;
-  int flag = 0;
-  int shift = 0x01;
+    int i;
+    int flag = 0;
+    int shift = 0x01;
 
-  FndSelect(position);
+    FndSelect(position);
 
-  for (i = 0; i < 8; i++) {
-    if (position == 2)
-      flag = ((FndFont[num] | 0x80) & shift);
-    else
-      flag = (FndFont[num] & shift);
-    digitalWrite(FndPin[i], flag);
-    shift <<= 1;
-  }
+    for (i = 0; i < 8; i++) {
+        if (position == 2)
+            flag = ((FndFont[num] | 0x80) & shift);
+        else
+            flag = (FndFont[num] & shift);
+        digitalWrite(FndPin[i], flag);
+        shift <<= 1;
+    }
 }
 
+void* FndThread(void* arg) {
+    while (1) {
+        int i;
+        int data[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-void FndThread(void* arg) {
-   while(1) {
-      int i;
-      int data[10]={0,1,2,3,4,5,6,7,8,9};
+        pthread_mutex_lock(&mutex);  // 뮤텍스 잠금
 
-
-      for(i=0; i<6; i++) {
-         FndDisplay(i, data[(timer/(int)pow(10,i))%10]);
-         delay(1);
-         if((i+timer)%2 == 0)
+        for (i = 0; i < 6; i++) {
+            FndDisplay(i, data[(timer / (int)pow(10, i)) % 10]);
             delay(1);
-      }
+            if ((i + timer) % 2 == 0)
+                delay(1);
+        }
 
-      delay(1);
+        pthread_mutex_unlock(&mutex);  // 뮤텍스 해제
 
-      if(stop == true) { continue; }
+        delay(1);
 
-      timer++;
+        if (stop == true) { continue; }
 
-      if(timer>=1000000) {
-         timer = 0;
-      }
-   }
+        pthread_mutex_lock(&mutex);  // 뮤텍스 잠금
+        timer++;
+        if (timer >= 1000000) {
+            timer = 0;
+        }
+        pthread_mutex_unlock(&mutex);  // 뮤텍스 해제
+    }
 }
 
+void IPCThread() {
+    int fd;
+    char buff[BUFF_SIZE];
 
-void IPCThread(void* arg) {
-	int fd;
-	char buff[BUFF_SIZE];
-	
-  fd = open(FIFO_FILE, O_RDONLY);
+    while (1) {
+        fd = open(FIFO_FILE, O_RDONLY);
 
-	while(1) {
-		memset(buff, 0, BUFF_SIZE);
-		read(fd, buff, BUFF_SIZE);
+        memset(buff, 0, BUFF_SIZE);
+        read(fd, buff, BUFF_SIZE);
 
-    if(strcmp(buff, "") != 0) {
-      // perror(buff);
+        if (strcmp(buff, "") != 0) {
+            if (strcmp(buff, "2") == 0) {
+                pthread_mutex_lock(&mutex);  // 뮤텍스 잠금
+                stop = true;
+                pthread_mutex_unlock(&mutex);  // 뮤텍스 해제
+                perror("stop error! : ");
+            }
+            else if (strcmp(buff, "3") == 0) {
+                pthread_mutex_lock(&mutex);  // 뮤텍스 잠금
+                timer = 0;
+                pthread_mutex_unlock(&mutex);  // 뮤텍스 해제
+                perror("clear error! : ");
+            }
+            else if (strcmp(buff, "1") == 0) {
+                pthread_mutex_lock(&mutex);  // 뮤텍스 잠금
+                stop = false;
+                pthread_mutex_unlock(&mutex);  // 뮤텍스 해제
+                perror("press start");
+            }
+            else {
+                perror("read buff error! : ");
+                exit(1);
+            }
+        }
 
-      if(strcmp(buff, "2") == 0) {
-        stop = true;
-        // write(fd, buff, strlen(buff));
-        perror("press stop");
-      }
-
-      else if(strcmp(buff, "3") == 0) {
-        timer = 0;
-        // write(fd, buff, strlen(buff));
-        perror("press clear");
-      }
-
-      else {
-        perror("read buff error!------------------");
-        exit(1);
-      }
+        close(fd);
+        delay(10);
     }
-
-    close(fd);
-    delay(10);
-	}
-
-
-  //char *querString, *delim;
-  //char mydata[1]="\0";
-  //int len = 0;
-  //int i = 0;
-
-   //while(1) {
-    //querString = getenv("QUERY_STRING");
-    //delim = strchr(querString, '=');
-    //strncpy(mydata,delim + 1,1);
-
-    //printf("<p>querString : %s</p>", querString);
-    //printf("<p>delim : %s</p>", delim);
-    //printf("<p>mydata : %s</p>", mydata);
-
-    // for(i=0; i<sizeof(delim); i++) {
-    //    delim[i] = delim[i+1];
-    // }
-    // delim[sizeof(delim)] = "/0";
-
-    /*
-    if(delim) {
-        len = strlen(mydata);
-        *data = malloc(sizeof(mydata) *len);
-        strcpy(*data, mydata);
-    } else {
-        len = 1;
-        *data = malloc(sizeof(mydata) *len);
-        **data = "\0";
-    }
-    */
-
-    /*
-      if(strcmp(mydata, "2") == 0) {
-      // stop
-         stop = true;
-      }
-
-      else if(strcmp(mydata, "3") == 0) {
-      // clear
-         timer = 0;
-      }
-
-      else if(strcmp(mydata, "1") == 0) {
-      // start
-         // perror("read buff error!");
-         // exit(1);
-         stop = false;
-      }
-    */
-   //}
 }
-
 
 int main() {
-  setbuf(stdout, NULL);
+    setbuf(stdout, NULL);
 
-	printf("Content-type:text/html\n\n");
-	printf("<html>\n<head>\n<title>STOP WATCH!</title>\n</head>\n");
-	printf("<body>\n<p>Start</p>\n");
+    printf("Content-type:text/html\n\n");
+    printf("<html>\n<head>\n<title>STOP WATCH PROGRAM</title>\n</head>\n");
+    printf("<body>\n<p>Stop Watch</p>\n</br>\n");
 
-	int pid;
+    pthread_t fndThread;
+    int result;
 
-  Setup();
+    if (pthread_mutex_init(&mutex, NULL) != 0) {
+        printf("Mutex initialization failed.\n");
+        return 1;
+    }
 
-	//pid = fork();
-	//if(pid > 0) {
-	//	Setup();
-	//	perror("Init error: ");
-	//}
+    Setup();
 
-	//else if(pid == 0) {
-		pthread_t p_thread[2];
-		int thr_id;
-		int status;
-		int a = 1;
-		int b = 2;
+    result = pthread_create(&fndThread, NULL, FndThread, NULL);
+    if (result != 0) {
+        printf("Failed to create FndThread.\n");
+        return 1;
+    }
 
-		thr_id = pthread_create(&p_thread[0], NULL, FndThread, (void *)&a);
+    IPCThread();
 
-		if(thr_id < 0) {
-			perror("thread create error: ");
-			exit(1);
-		}
+    pthread_join(fndThread, NULL);
+    pthread_mutex_destroy(&mutex);
 
-		thr_id = pthread_create(&p_thread[1], NULL, IPCThread, (void *)&b);
+    printf("</body>\n</html>");
 
-		if(thr_id <0) {
-			perror("thread create error: ");
-			exit(1);
-		}
-
-		pthread_join(p_thread[0], (void **)&status);
-		pthread_join(p_thread[1], (void **)&status);
-
-	//}
-
-	//else if(pid == -1) {
-	//	perror("fork error: ");
-	//	exit(1);
-	//}
-
-	printf("</body>\n</html>");
-
-	return 0;
+    return 0;
 }
