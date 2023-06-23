3d0efb91345e5f5a500064ea7da4634743c7b620
clair1162 <clair1162@khu.ac.kr>
2023-06-23 01:46:25 +0000

    park, start, clear, stop

---
+++ stop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <wiringPi.h>
+
+#define FIFO_FILE	"/tmp/fifo"
+#define BUFF_SIZE	1024
+
+int main() {
+	int fd;
+	char *str = "2";
+
+	if(-1 ==(fd = open(FIFO_FILE, O_WRONLY))) {
+		perror("open() error!");
+	}
+
+	write(fd, str, strlen(str));
+	close(fd);
+
+	return 0;
+}
